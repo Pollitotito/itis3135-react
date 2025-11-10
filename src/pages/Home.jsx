11799b8bd208b299da7151ea10570c24e829344a
Diego Mendoza <146658873+Pollitotito@users.noreply.github.com>
2025-11-10 12:07:23 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="front-home">
       <main>
+        <h2 className="center">Home</h2>
         <img
           className="kame-house"
           src="/image/KameHouse.webp"
